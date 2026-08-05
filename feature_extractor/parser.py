980664fce91e031794c9e019b6ed87312d0a56b1
rasik583 <rasikakante@gmail.com>
2026-08-05 14:24:17 +0530

    feat: add image, CSS and JavaScript feature extraction

--- feature_extractor/parser.py
+++ feature_extractor/parser.py
@@ -7,3 +7,12 @@ def get_title(soup):
         return soup.title.string.strip()
 
     return "No Title"
+
+def get_image_count(soup):
+    return len(soup.find_all("img"))
+
+def get_css_count(soup):
+    return len(soup.find_all("link", rel="stylesheet"))
+
+def get_js_count(soup):
+    return len(soup.find_all("script", src=True))
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from feature_extractor.extractor import fetch_website
-from feature_extractor.parser import get_title
-
+from feature_extractor.parser import (
+    get_title,
+    get_image_count,
+    get_css_count,
+    get_js_count
+)
 url = "https://github.com"
 
 soup = fetch_website(url)
 
 print("Website Title:", get_title(soup))
+print("Image Count:", get_image_count(soup))
+print("CSS Files:", get_css_count(soup))
+print("JavaScript Files:", get_js_count(soup))
